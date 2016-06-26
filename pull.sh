076e1b202289b1acc12ea7faa0beb753e7ea6556
Andrew Backes <rbackes@ucla.edu>
2016-06-26 12:03:47 -0700

    need sudo for that dir

--- pull.sh
+++ pull.sh
@@ -4,9 +4,9 @@ dir="/usr/local/autonoma/"
 
 if [ -d "$dir" ]; then
     cd "$dir"
-    git checkout master
-    git pull
+    sudo git checkout master
+    sudo git pull
 else
     cd "/usr/local/"
-    git clone https://github.com/andrewbackes/autonoma
+    sudo git clone https://github.com/andrewbackes/autonoma
 fi
