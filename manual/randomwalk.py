559150bbe85061bb297cec01a61f1b2ee135f88e
Andrew Backes <rbackes@ucla.edu>
2016-06-27 19:16:48 -0700

    try roaming

---
+++ manual/randomwalk.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import time
+import RPi.GPIO as gpio
+import move
+import echo
+
+increment = 0.1
+unit = 'in'
+
+def move_until_blocked(dist):
+    print("moving")
+    blocked = false
+    while True
+        d = echo.distance(echo.sensors['front'], unit)
+        print("Nearest object: " + str(d)) 
+        if d <= dist:
+            return
+        move.forward(increment)
+
+
+def find_unblocked_path(dist):
+    print("rotating")
+    open_positions = 0
+    while open_positions < 3:
+        move.clockwise(increment)
+        d = echo.distance(echo.sensors['front'], unit)
+        if d < dist*2:
+            open_positions += 1
+        else:
+            open_positions = 0
+    move.counter_clockwise(increment)
+    
+
+def walk():
+    while True:
+        move_until_blocked()
+        time.sleep(0.1)
+        find_unblocked_path
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    walk() 
