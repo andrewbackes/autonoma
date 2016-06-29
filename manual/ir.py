ac40286ad1c96d146406ef6b8d5eff75f83c0db6
Andrew Backes <rbackes@ucla.edu>
2016-06-28 21:06:54 -0700

    figure out which sensor is which

--- manual/ir.py
+++ manual/ir.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as gpio
+import time
 
 sensors = {
     'left': 32,
@@ -11,3 +12,15 @@ def blocked(sensor):
     gpio.setmode(gpio.BOARD)
     gpio.setup(sensor, gpio.IN)
     return gpio.input(sensor) == 0
+
+
+def dashboard():
+    while True:
+        l = blocked(sensors['left'])
+        r = blocked(sensors['right'])
+        block = {
+            'left': l,
+            'right': r
+        }
+        print(block)
+        time.sleep(0.5)
