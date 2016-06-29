78082d51c9bd2391c4e4b6520a988ade9a021ee0
Andrew Backes <rbackes@ucla.edu>
2016-06-28 21:24:13 -0700

    correct id for sensor

--- manual/ir.py
+++ manual/ir.py
@@ -4,14 +4,15 @@ import RPi.GPIO as gpio
 import time
 
 sensors = {
-    'left': 32,
-    'right': 36
+    'left': 36,
+    'right': 32
 }
 
-def blocked(sensor):
+def blocked():
     gpio.setmode(gpio.BOARD)
-    gpio.setup(sensor, gpio.IN)
-    return gpio.input(sensor) == 0
+    gpio.setup(sensors['left'], gpio.IN)
+    gpio.setup(sensors['right'], gpio.IN)
+    return gpio.input(sensors['left']) == 0 and gpio.input(sensors['right']) == 0 
 
 
 def dashboard():
