2adcb655bf5928beb7a66f5e91ed44faad882e1f
Andrew Backes <rbackes@ucla.edu>
2016-06-30 08:12:46 -0700

    added pwm to movement

--- manual/move.py
+++ manual/move.py
@@ -3,38 +3,64 @@
 import time
 import RPi.GPIO as gpio
 
+pwm = {
+    'left': 38,
+    'right': 40
+}
+
+driver = {
+    'in1': 15,
+    'in2': 13,
+    'in3': 11,
+    'in4': 77,
+}
+
 def init():
     gpio.setmode(gpio.BOARD)
     gpio.setup(7, gpio.OUT)
     gpio.setup(11, gpio.OUT)
     gpio.setup(13, gpio.OUT)
     gpio.setup(15, gpio.OUT)
+    gpio.setup(40, gpio.OUT)
+    gpio.setup(38, gpio.OUT)
+    gpio.setup(pwm['left'], gpio.OUT)
+    gpio.setup(pwm['right'], gpio.OUT)
 
-def toggle(a, b, c, d, t):
+def run_at_speed(t, speed=100):
+    p = {}
+    p['left'] = gpio.PWM(pwm['left'], 100) # 100 hz
+    p['right'] = gpio.PWM(pwm['right'], 100)
+    p['left'].start(speed)
+    p['right'].start(speed)
+    time.sleep(t)
+    p['left'].stop()
+    p['right'].stop()
+
+def toggle(a, b, c, d, t, speed=100):
     init()
     gpio.output(7, a)
     gpio.output(11, b)
     gpio.output(13, c)
     gpio.output(15, d)
-    time.sleep(t)
+    run_at_speed(t, speed)
     gpio.cleanup()
 
-def forward(t):
+def forward(t, speed=100):
     toggle(False, True, True, False, t)
 
-def backward(t):
+def backward(t, speed=100):
     toggle(True, False, False, True, t)
 
-def turn_left(t):
+def turn_left(t, speed=100):
     toggle(False, True, False, False, t)
 
-def turn_right(t):
+def turn_right(t, speed=100):
     toggle(True, True, True, False, t)
 
-def counter_clockwise(t):
+def counter_clockwise(t, speed=100):
     toggle(False, True, False, True, t)
 
-def clockwise(t):
+def clockwise(t, speed=100):
     toggle(True, False, True, False, t)
 
 
