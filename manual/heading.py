f7400d5739779f65615c9431d627374c858a5ff7
Andrew Backes <rbackes@ucla.edu>
2016-07-01 22:25:04 -0700

    woops

--- manual/heading.py
+++ manual/heading.py
@@ -10,15 +10,18 @@ os.environ["PYTHONPATH"] += ":/usr/local/autonoma/manual/quick2wire-python-api"
 from i2clibraries import i2c_hmc5883l
 
 def init():
+    hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)
     hmc5883l.setContinuousMode()
     hmc5883l.setDeclination(13, 30)
+    return hmc5883l
 
 def degrees():
-    init()
+    hmc5883l = init()
     (h, m) = hmc5883l.getHeading()
     return h
 
 if __name__ == "__main__":
+    hmc5883l = init()
     while True:
         print(hmc5883l)
         time.sleep(1.0)
