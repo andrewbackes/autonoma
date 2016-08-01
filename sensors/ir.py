00341faf80cd53a08815644a828dce0596211ac0
Andrew Backes <rbackes@ucla.edu>
2016-07-31 17:36:54 -0700

    ir distance sensor

--- sensors/ir.py
+++ sensors/ir.py
@@ -2,6 +2,8 @@
 
 import RPi.GPIO as gpio
 import time
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
 sensors = {
     'left': 36,
@@ -19,3 +21,12 @@ def blocked(sensor="all"):
         blocked = gpio.input(sensors[sensor]) == 0
     gpio.cleanup()
     return blocked
+
+def distance():
+    # Hardware SPI configuration:
+    SPI_PORT   = 0
+    SPI_DEVICE = 0
+    mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+    value = mcp.read_adc(0)
+    cm = (70/837)*value
+    return cm
