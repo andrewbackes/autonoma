54294afe090c53afa482789cd28735774b858d0f
andrewbackes <rbackes@ucla.edu>
2018-03-17 19:36:29 -0700

    use correct sensor name

--- cmd/bot/lib/orientation.py
+++ cmd/bot/lib/orientation.py
@@ -14,7 +14,7 @@ def heading():
 if __name__ == "__main__":
     while True:
         # Read the Euler angles for heading, roll, pitch (all in degrees).
-        heading, roll, pitch = bno.read_euler()
+        heading, roll, pitch = bno055.read_euler()
         # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
         print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} ' +
               'Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
