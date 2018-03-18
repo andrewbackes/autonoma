1452c5719e7b237c52980ff105f407eb159c90c1
andrewbackes <rbackes@ucla.edu>
2018-03-17 20:16:22 -0700

    Make servo a class

--- cmd/bot/lib/servo.py
+++ cmd/bot/lib/servo.py
@@ -3,45 +3,45 @@
 import time
 import RPi.GPIO as gpio
 
-servo_pin = 37
-
-freq = 50
-
-rightpos = 0.5
-leftpos = 2.5
-middlepos = (leftpos - rightpos) / 2 + leftpos
-
-msPerCylce = 1000 / freq
-
-pwm = None
+# middlepos = (leftpos - rightpos) / 2 + leftpos
+default_config = {
+    'servo_pin': 37,
+    'frequency': 50,
+    'calibration': {
+        'left': 0.5,
+        'right': 2.5
+    }
+}
 
 
-def init():
-    gpio.setup(servo_pin, gpio.OUT)
-    pwm = gpio.PWM(servo_pin, freq)
+class Servo:
 
+    def __init__(self, config=default_config):
+        if gpio.getmode() != gpio.BOARD:
+            gpio.setmode(gpio.BOARD)
+        self.msPerCylce = 1000 / config['frequency']
+        gpio.setup(config['servo_pin'], gpio.OUT)
+        self.pwm = gpio.PWM(config['servo_pin'], config['frequency'])
 
-def pos(deg):
-    pos = ((leftpos - rightpos) / 180)
-    return pos * deg + leftpos
+    def pos(self, deg):
+        pos = ((leftpos - rightpos) / 180)
+        return pos * deg + leftpos
 
-
-def move(deg):
-    interval = pos((deg + 90) * -1)
-    dutyPerc = interval * 100 / msPerCylce
-    if not pwm:
-        print("must initialize servo.init()")
-        return
-    pwm.start(dutyPerc)
-    time.sleep(0.5)
-    pwm.stop()
+    def move(self, deg):
+        interval = pos((deg + 90) * -1)
+        dutyPerc = interval * 100 / msPerCylce
+        if not pwm:
+            print("must initialize servo.init()")
+            return
+        self.pwm.start(dutyPerc)
+        time.sleep(0.5)
+        self.pwm.stop()
 
 
 if __name__ == "__main__":
-    gpio.setmode(gpio.BOARD)
-    init()
+    servo = Servo()
     positions = [-90, -45, 0, 45, 90]
     for p in positions:
-        move(p)
+        servo.move(p)
         time.sleep(0.5)
     gpio.cleanup()
