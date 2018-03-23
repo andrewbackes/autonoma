727a0b784fca571649accccb0e78b5b60c91d714
andrewbackes <rbackes@ucla.edu>
2018-03-22 21:43:37 -0700

    Correct distance calc

--- cmd/bot/lib/ultrasonic.py
+++ cmd/bot/lib/ultrasonic.py
@@ -23,13 +23,13 @@ class UltraSonic:
 
     def __init__(self, config=default_config):
         self.config = config
-        print("H-bridge config:", config)
+        print("Ultrasonic config:", config)
         if gpio.getmode() != gpio.BOARD:
             gpio.setmode(gpio.BOARD)
         gpio.setup(self.config['trigger'], gpio.OUT)
         gpio.setup(self.config['echo'], gpio.IN)
 
-    def distance(self, measure='cm'):
+    def distance(self):
         gpio.output(self.config['trigger'], True)
         time.sleep(0.00001)  # specified wait time for this hardware
         gpio.output(self.config['trigger'], False)
@@ -42,16 +42,8 @@ class UltraSonic:
         lapsed = time.time() - start
         while gpio.input(self.config['echo']) == 1:
             lapsed = time.time() - start
-            if lapsed > 0.004:
-                return None
 
-        if measure == 'cm':
-            distance = lapsed / 0.000058
-        elif measure == 'in':
-            distance = lapsed / 0.000148
-        else:
-            raise ValueError('unsupported measurement')
-            distance = None
+        distance = (lapsed * 34300) / 2
 
         return distance
 
