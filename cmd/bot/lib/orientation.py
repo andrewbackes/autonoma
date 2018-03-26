325a6bb6845cebfe92faed328e62e32afd3c7cd8
andrewbackes <rbackes@ucla.edu>
2018-03-25 20:47:31 -0700

    Include bno055 self test

--- cmd/bot/lib/orientation.py
+++ cmd/bot/lib/orientation.py
@@ -9,10 +9,38 @@ class Orientation:
     def __init__(self):
         # use i2c
         self.bno055 = BNO055.BNO055()
+        self.__self_test()
+        self.__revision_info()
+
+    def __self_test(self):
+        print('Running BNO055 self test')
+        # Print system status and self test result.
+        status, self_test, error = self.bno055.get_system_status()
+        print('System status: {0}'.format(status))
+        print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+        # Print out an error if system status is in error mode.
+        if status == 0x01:
+            print('System error: {0}'.format(error))
+            print('See datasheet section 4.3.59 for the meaning.')
+
+    def __revision_info(self):
+        sw, bl, accel, mag, gyro = self.bno055.get_revision()
+        print('Software version:   {0}'.format(sw))
+        print('Bootloader version: {0}'.format(bl))
+        print('Accelerometer ID:   0x{0:02X}'.format(accel))
+        print('Magnetometer ID:    0x{0:02X}'.format(mag))
+        print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+
+    def read(self):
+        time.sleep(0.01)
+        # heading, roll, pitch = self.bno055.read_euler()
+        # sys, gyro, accel, mag = bno.get_calibration_status()
+        return self.bno055.read_euler(), bno.get_calibration_status()
 
     def heading(self):
         time.sleep(0.01)
-        heading, roll, pitch = self.bno055.read_euler()
+        euler, cal = read
+        heading, roll, pitch = euler
         return heading
 
 
