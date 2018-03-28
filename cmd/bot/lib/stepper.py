f4eb2a4f0d960341774eca182d84f44cfd25ec29
andrewbackes <rbackes@ucla.edu>
2018-03-27 21:58:00 -0700

    Add stepper

---
+++ cmd/bot/lib/stepper.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import time
+import RPi.GPIO as gpio
+
+#
+# Nema 17
+#  Note:
+#  - Coil #1: Red & Yellow wire pair. Coil #2 Green & Brown/Gray wire pair.
+#
+
+
+class Stepper:
+
+    CLOCKWISE = 1
+    COUNTER_CLOCKWISE = 0
+
+    _config = {
+        "gpio": {
+            "step": 36,
+            "dir": 32
+        },
+        "stepsPerRevolution": 200,  # 1.8 per step
+        "stepDelay": 1.0 / 200,
+    }
+
+    def __init__(self, config={}):
+        self._config.update(config)
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(self._config['gpio']['dir'], gpio.OUT)
+        gpio.setup(self._config['gpio']['step'], gpio.OUT)
+        gpio.output(self._config['gpio']['dir'], self.CLOCKWISE)
+
+    def one(self):
+        for x in range(self._config['stepsPerRevolution']):
+            gpio.output(self._config['gpio']['step'], gpio.HIGH)
+            time.sleep(self._config['gpio']['stepDelay'])
+            gpio.output(self._config['gpio']['step'], gpio.LOW)
+            time.sleep(self._config['gpio']['stepDelay'])
+
+
+if __name__ == "__main__":
+    print("Stepper test")
+    stepper = Stepper()
+    stepper.one()
