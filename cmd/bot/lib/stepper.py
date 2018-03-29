0bbc8efdf6f1f549e17b9ea335eef5c3e505b839
andrewbackes <rbackes@ucla.edu>
2018-03-28 21:58:16 -0700

    Micro-stepping and enable pin

--- cmd/bot/lib/stepper.py
+++ cmd/bot/lib/stepper.py
@@ -5,8 +5,10 @@ import RPi.GPIO as gpio
 
 #
 # Nema 17
-#  Note:
+#
 #  - Coil #1: Red & Yellow wire pair. Coil #2 Green & Brown/Gray wire pair.
+#  - Datasheet:
+#       https://cdn-shop.adafruit.com/product-files/324/C140-A+datasheet.jpg
 #
 
 
@@ -17,11 +19,23 @@ class Stepper:
 
     _config = {
         "gpio": {
-            "step": 36,
-            "dir": 32
+            "step":     36,
+            "dir":      32,
+            "ms1":      16,  # GPIO-23
+            "ms2":      22,  # GPIO-25
+            "ms3":      18,  # GPIO-24
+            "enable":   12   # GPIO-18
         },
         "stepsPerRevolution": 200,  # 1.8 per step
         "stepDelay": 1.0 / 200,
+        "resolution": {
+            #        MS1,          MS2,           MS3
+            'Full': (gpio.LOW,     gpio.LOW,      gpio.LOW),
+            'Half': (gpio.HIGH,    gpio.LOW,      gpio.LOW),
+            '1/4':  (gpio.LOW,     gpio.HIGH,     gpio.LOW),
+            '1/8':  (gpio.HIGH,    gpio.HIGH,     gpio.LOW),
+            '1/16': (gpio.HIGH,    gpio.HIGH,     gpio.HIGH),
+        },
     }
 
     def __init__(self, config={}):
@@ -30,6 +44,21 @@ class Stepper:
         gpio.setup(self._config['gpio']['dir'], gpio.OUT)
         gpio.setup(self._config['gpio']['step'], gpio.OUT)
         gpio.output(self._config['gpio']['dir'], self.CLOCKWISE)
+        gpio.setup(self._config['gpio']['enable'], gpio.OUT)
+        # for micro-stepping:
+        self._mode = (
+            self._config['gpio']['m1'],
+            self._config['gpio']['m2'],
+            self._config['gpio']['m3']
+        )
+        gpio.setup(self._mode, gpio.OUT)
+        gpio.output(self._mode, self._config['resolution']['1/16'])
+
+    def enable(self):
+        gpio.output(self._config['gpio']['enable'], gpio.LOW)
+
+    def disable(self):
+        gpio.output(self._config['gpio']['enable'], gpio.HIGH)
 
     def one(self):
         for x in range(self._config['stepsPerRevolution']):
@@ -43,3 +72,4 @@ if __name__ == "__main__":
     print("Stepper test")
     stepper = Stepper()
     stepper.one()
+    gpio.cleanup()
